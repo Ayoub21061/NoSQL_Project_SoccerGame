08595f0a7bc53a276b2b652e72bdbe9f411797ed
Ibtihal-mrn <mourninibtihal@gmail.com>
2025-10-03 23:37:18 +0200

    test DB + player de base

---
+++ Backend/main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from Routes.Player_routes import player_bp  # on importe le Blueprint des routes Player
+
+app = Flask(__name__)
+
+# On enregistre le blueprint des joueurs
+app.register_blueprint(player_bp, url_prefix="/players")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5001)
