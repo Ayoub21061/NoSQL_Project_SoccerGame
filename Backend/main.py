77ab8a2eed50bbaed17c765784832c6cc9f88ed5
Ayoub21061 <21061@ecam.be>
2025-10-04 19:46:27 +0200

    Added route for achievements

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from Routes.Player_routes import player_bp  # on importe le Blueprint des routes Player
 from Routes.Gamesmode_routes import games_bp  # on importe le Blueprint des routes Gamesmode
+from Routes.Achievements_routes import achievements_bp  # on importe le Blueprint des routes Achievements
+
 
 app = Flask(__name__)
 
@@ -8,6 +10,8 @@ app = Flask(__name__)
 app.register_blueprint(player_bp, url_prefix="/players")
 # On enregistre le blueprint des modes de jeu
 app.register_blueprint(games_bp, url_prefix="/games")
+# On enregistre le blueprint des succès
+app.register_blueprint(achievements_bp, url_prefix="/achievements")
 
 
 if __name__ == "__main__":
