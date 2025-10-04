222990bf16b76f38748ddd61e98fb182b913f399
Ayoub21061 <21061@ecam.be>
2025-10-04 18:29:32 +0200

    Added route for game modes

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from Routes.Player_routes import player_bp  # on importe le Blueprint des routes Player
+from Routes.Gamesmode_routes import games_bp  # on importe le Blueprint des routes Gamesmode
 
 app = Flask(__name__)
 
 # On enregistre le blueprint des joueurs
 app.register_blueprint(player_bp, url_prefix="/players")
+# On enregistre le blueprint des modes de jeu
+app.register_blueprint(games_bp, url_prefix="/games")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
