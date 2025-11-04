6e6ca884e4b7c03b69a077a8a75125182aaf854d
Ayoub21061 <21061@ecam.be>
2025-11-04 20:37:21 +0100

    New player_routes.py

--- Backend/Routes/Player_routes.py
+++ Backend/Routes/Player_routes.py
@@ -258,6 +258,11 @@ def update_user_items(username):
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
+
+from bson import ObjectId
+
+from bson import ObjectId
+
 @player_bp.route("/username/<username>/removeItem/<item_id>", methods=["DELETE"])
 def remove_item_from_user(username, item_id):
     try:
